1626c3ffb768cb7cd7de64ff4adfb1938552c31b
vaishnavimore23 <vaishnavimore23@outlook.com>
2024-04-03 17:47:12 -0700

    Backend done -- Integrated with MongoDB

---
+++ movies/src/main/java/com/example/movies/MovieController.java
@@ -0,0 +1,34 @@
+package com.example.movies;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/V1/movies")
+public class MovieController {
+
+    @Autowired
+    private  MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getallMovies(){
+       return new ResponseEntity<List<Movie>>(movieService.AllMovies(),HttpStatus.OK) ;
+
+    }
+
+    @GetMapping("/{imdbId}")
+    public  ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),HttpStatus.OK);
+    }
+
+}
---
+++ movies/src/main/java/com/example/movies/MovieRepository.java
@@ -0,0 +1,17 @@
+package com.example.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import  com.example.movies.Movie;
+
+import java.util.Optional;
+
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+
+
+}
